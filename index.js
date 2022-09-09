a6d616b99673ea6688d5c0aadb37f48887b29ba1
Chris Martin <cmartin@cloudflare.com>
2022-09-09 14:09:08 -0500

    Assignment 1 commit

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+	// Return a new Response based on a URL's hostname
+ 	const url = new URL(request.url);
+ 	var data = "Thanks for making a POST"
+ 	var html = "Welcome CM-Code-Club-2-Cohort-4"
+  
+// On HTTP method
+	if (request.method === 'POST') {
+		
+		request.content = JSON.stringify(data)
+		request.type = 'application/json'
+  	}else{
+	    request.content = html
+	    request.type = 'text/html'
+    
+  }
+  return new Response(request.content, {status:200, headers:{'content-type': request.type}})
+}
